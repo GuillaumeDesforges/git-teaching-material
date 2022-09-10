def2be52b136fd9a314aae0b711df05fbf040720
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2022-09-10 17:28:19 +0200

    Pin nixpkgs rev

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   nixConfig.bash-prompt-prefix = "(nix) ";
 
+  inputs.nixpkgs.url = "nixos/nixpkgs/e34c5379866833f41e2a36f309912fa675d687c7";
   inputs.flake-utils = {
     url = "github:numtide/flake-utils";
     inputs.nixpkgs.follows = "nixpkgs";
