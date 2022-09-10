4eaa73a890e9c30cb7c6cdda499c537fb55a437a
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2022-09-10 17:31:12 +0200

    fix nixpkgs url in flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   nixConfig.bash-prompt-prefix = "(nix) ";
 
-  inputs.nixpkgs.url = "nixos/nixpkgs/e34c5379866833f41e2a36f309912fa675d687c7";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/e34c5379866833f41e2a36f309912fa675d687c7";
   inputs.flake-utils = {
     url = "github:numtide/flake-utils";
-    inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
