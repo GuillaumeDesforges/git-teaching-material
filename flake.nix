4f94b462ece93cbbe6c79c4efd4674d77c9bee98
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-08-18 22:57:15 +0200

    Update nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   # to be changed after PR is merged
   # https://github.com/NixOS/nixpkgs/pull/244504
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils, ... }:
@@ -11,12 +11,12 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        packages.default = pkgs.runCommand "git-teaching-material" {} ''
+        packages.default = pkgs.runCommand "git-teaching-material" { } ''
           mkdir -p $out
           cp -r ${self.packages.${system}.practical-session} $out/practical-session
         '';
         # packages.presentation = pkgs.callPackage ./presentation {};
-        packages.practical-session = pkgs.callPackage ./practical-session {};
+        packages.practical-session = pkgs.callPackage ./practical-session { };
       }
     );
 }
